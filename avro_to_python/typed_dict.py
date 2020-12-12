b868c017fc6e329902e54c4942c1f5b1b91282d1
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2020-12-12 14:33:17 -0500

    Add support for nested types

--- avro_to_python/typed_dict.py
+++ avro_to_python/typed_dict.py
@@ -1,6 +1,8 @@
 from .generate_typed_dict import GenerateTypedDict
 from .schema_mapping import prim_to_type
+import ast
 import json
+import astor
 
 test_schema = json.loads(
     """
@@ -40,22 +42,38 @@ def is_nested(field):
     return False
 
 
-def type_for_record(schema):
-    our_type = GenerateTypedDict(schema["name"])
-    for field in schema["fields"]:
-        if is_nested(field):
-            continue
-        name = field["name"]
-        if is_nullable(field):
-            type = field["type"][0]
-            print(prim_to_type[type])
-            our_type.add_optional_element(name, prim_to_type[type])
-        else:
-            type = field["type"]
-            our_type.add_required_element(name, prim_to_type[type])
-    return our_type
+def type_for_schema(schema):
+    body = []
+    tree = ast.Module(body)
+    body = tree.body
+
+    def type_for_schema_record(record_schema):
+        our_type = GenerateTypedDict(record_schema["name"])
+        for field in record_schema["fields"]:
+            name = field["name"]
+            if is_nested(field):
+                nested = type_for_schema_record(field["type"])
+                body.append(nested.tree)
+                if is_nullable(field):
+                    our_type.add_optional_element(name, nested.name)
+                else:
+                    our_type.add_required_element(name, nested.name)
+                continue
+            if is_nullable(field):
+                type = field["type"][0]
+                our_type.add_optional_element(name, prim_to_type[type])
+            else:
+                type = field["type"]
+                our_type.add_required_element(name, prim_to_type[type])
+        return our_type
+
+    main_type = type_for_schema_record(schema)
+    body.append(main_type.tree)
+    return tree
 
 
 def typed_dict():
-    new_type = type_for_record(test_schema)
-    print(new_type.get_code())
+    new_type = type_for_schema(test_schema)
+    imports = "from typing import TypedDict\n\n"
+    types = astor.to_source(new_type)
+    print(imports + types)
