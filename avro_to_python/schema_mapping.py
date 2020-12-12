fa5614c30ab52813e224cea95954b44ea9e00c8f
Dan Green-Leipciger <dangreen.tlv@gmail.com>
2020-12-12 13:44:02 -0500

    Add mapping of avro types to python types

---
+++ avro_to_python/schema_mapping.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+prim_to_type = {
+    "null": "None",
+    "boolean": "bool",
+    "int": "int",
+    "long": "int",
+    "float": "float",
+    "double": "float",
+    "bytes": "bytes",
+    "string": "str",
+}
