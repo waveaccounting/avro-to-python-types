66aa9678ec727ae9cd0403d5febd342be6621186
Wendell Nichols <wnichols@waveapps.com>
2021-11-01 09:34:57 -0600

    Add the "map" and "fixed" avro types.

--- avro_to_python_types/__init__.py
+++ avro_to_python_types/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.11.2"
+__version__ = "0.12.0"
 from .typed_dict_from_schema import (
     typed_dict_from_schema_file,
     typed_dict_from_schema_string,
--- tests/test_schema_to_typed_dict.py
+++ tests/test_schema_to_typed_dict.py
@@ -8,6 +8,7 @@ import snapshottest
 test_schema_files = glob.glob("tests/test_cases/*.avsc")
 expandable_schema_files = glob.glob("tests/test_cases_expandable/*.avsc")
 shopping_cart_files = glob.glob("tests/test_shopping_cart/*.avsc")
+handshake_files = glob.glob("tests/test_handshake/*.avsc")
 
 
 class SnapshotTypedDictFromSchemaFile(snapshottest.TestCase):
@@ -49,4 +50,12 @@ class SnapshotTypedDictFromSchemaString(snapshottest.TestCase):
                 avro_schema = f.read()
             test_output = typed_dict_from_schema_string(avro_schema)
             schema_name = test_json_file.rsplit("/", 1)[-1]
+            self.assertMatchSnapshot(test_output, schema_name)
+
+
+class SnapshotTypedDictMapAndFixedFromSchemaFile(snapshottest.TestCase):
+    def test_array_map_schemas(self):
+        for test_schema_file in handshake_files:
+            test_output = typed_dict_from_schema_file(test_schema_file)
+            schema_name = test_schema_file.rsplit("/", 1)[-1]
             self.assertMatchSnapshot(test_output, schema_name)
